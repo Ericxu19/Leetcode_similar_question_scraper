8e6a69579c404385ac75fb173be3fbd49b6324d2
eric <xihh01@gmail.com>
2020-09-06 22:15:27 -0700

    remove useless

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from utils import *
-import epub_writer
 import json
 import re
 
